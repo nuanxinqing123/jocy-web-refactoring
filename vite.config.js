4efe39307b865dc47f2d53b60794f242cb16d8cf
Nuanxinqing <nuanxinqing@gmail.com>
2025-03-23 15:32:57 +0800

    init commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,79 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vitePluginBundleObfuscator from "vite-plugin-bundle-obfuscator";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    build: {
+        outDir: "dist",
+        assetsDir: "assets", //指定静态资源存放路径
+        minify: "esbuild", // 默认
+        rollupOptions: {
+            output: {
+                entryFileNames: `assets/[name].[hash].js`,
+                chunkFileNames: `assets/[name].[hash].js`,
+                assetFileNames: "assets/[ext]/[hash].[ext]",
+            },
+        }, // 打包后文件的存放路径
+    },
+    esbuild: {
+        drop: ["console", "debugger"], //打包去除
+    },
+    server: {
+        //设为 true 时若端口已被占用则会直接退出，而不是尝试下一个可用端口
+        strictPort: false,
+        //服务器启动时自动在浏览器中打开应用程序,当此值为字符串时，会被用作 URL 的路径名
+        open: true,
+        proxy: {
+            "/api": {
+                target: "https://jocy-api.6b7.xyz/app",
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ""),
+            },
+        },
+    },
+    define: {
+        'process.env': {
+            BASE_API: '',
+        },
+    },
+    plugins: [
+        vue(),
+        vitePluginBundleObfuscator({
+            excludes: ["*.css"],
+            enable: true,
+            log: true,
+            autoExcludeNodeModules: false,
+            threadPool: false,
+            options: {
+                compact: true,
+                controlFlowFlattening: true,
+                controlFlowFlatteningThreshold: 1,
+                deadCodeInjection: false,
+                debugProtection: false,
+                debugProtectionInterval: 0,
+                disableConsoleOutput: false,
+                identifierNamesGenerator: "hexadecimal",
+                log: false,
+                numbersToExpressions: false,
+                renameGlobals: false,
+                selfDefending: true,
+                simplify: true,
+                splitStrings: false,
+                stringArray: false,
+                stringArrayCallsTransform: false,
+                stringArrayCallsTransformThreshold: 0.5,
+                stringArrayEncoding: [],
+                stringArrayIndexShift: true,
+                stringArrayRotate: true,
+                stringArrayShuffle: true,
+                stringArrayWrappersCount: 1,
+                stringArrayWrappersChainedCalls: true,
+                stringArrayWrappersParametersMaxCount: 2,
+                stringArrayWrappersType: "variable",
+                stringArrayThreshold: 0.75,
+                unicodeEscapeSequence: false,
+            },
+        }),
+    ],
 })
